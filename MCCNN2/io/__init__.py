8949d64bd51f8c609057a0d0223dcf494bf51067
Michael Schelling <michael-1.schelling@uni-ulm.de>
2020-06-10 15:31:52 +0200

    updated point could io to use tf.io.gfile

--- MCCNN2/io/__init__.py
+++ MCCNN2/io/__init__.py
@@ -1,2 +1,3 @@
 from .points import load_points_from_file
 from .points import load_batch_of_points
+from .points import load_batch_of_meshes
--- MCCNN2/io/points.py
+++ MCCNN2/io/points.py
@@ -16,58 +16,131 @@
 
 import os
 import tensorflow as tf
-import trimesh
-from trimesh import Scene
-from trimesh import Trimesh
+from tensorflow_graphics.io.triangle_mesh import load as load_mesh
 
 
-def load_points_from_file(filename, delimiter=',', dimension = 3, dtype=tf.float32, name=None):
+def load_points_from_file(filename, delimiter=',', dimension = 3, dtype=tf.float32):
+  """ Loads point clouds with features from ASCII files using tf.io.gfile.GFile()
 
-    points = []
-    features =[]
-    with open(filename,'r') as in_file:
-      for line in in_file:
-        line_elements = line[:-1].split(delimiter)
-        points.append(line_elements[0:dimension])
-        if len(line_elements)>3:
-          features.append(line_elements[dimension:])
-    with tf.compat.v1.name_scope(name, "point loader", [points, features, dtype]):
+  Args:
+    filename: `string` path to the file
+    delimiter: `string` delimiter that separates the points in the file
+    dimension: `int` D1 , the first D1 elements in each line are treated as point coordinates, the rest as features
+    dtype: `tf.dtype` of the output tensors
+
+  Returns:
+    points: A `Tensor` of shape [N,D1] and type dtype
+    features: A `Tensor` of shape [N,D2] and type dtype
+  
+  Raises:
+    TypeError: if filename ist not of type 'string'
+    FileNotFoundError: if filename does not exist
+
+  """
+
+  points = []
+  features =[]
+  if isinstance(filename, str):
+    if tf.io.gfile.exists(filename):
+      with tf.io.gfile.GFile(filename,'r') as in_file:
+        for line in in_file:
+          line_elements = line[:-1].split(delimiter)
+          points.append(line_elements[0:dimension])
+          if len(line_elements)>3:
+            features.append(line_elements[dimension:])
+
       points = tf.convert_to_tensor(value=points)
       features = tf.convert_to_tensor(value=features)
       points = tf.strings.to_number(input=points, out_type=dtype)
       features = tf.strings.to_number(input=features, out_type=dtype)
-    return points, features
+      return points, features
+    else:
+      raise FileNotFoundError("No such file: %s"%filename)
+  else:
+    raise TypeError("'filename' must be of type 'string'")
 
-def load_batch_of_points(filenames, batch_shape=[-1], delimiter=',', point_dimension=3, dtype=tf.float32, name=None):
-    with tf.compat.v1.name_scope(name, "load batch of points", [filenames, batch_shape, delimiter, point_dimension, dtype]):
-      batch_size = len(filenames)
-      if tf.reduce_prod(batch_shape)!=batch_size:
-        raise ValueError('Invalid batch shape %s for batch size %s'%(batch_shape, batch_size))
-      points = []
-      features = []
-      max_num_points = 0
-      sizes = []
-      for filename in filenames:
-        curr_points, curr_features = load_points_from_file(filename=filename, delimiter=delimiter, dtype=dtype)
-        points.append(curr_points)
-        features.append(curr_features)
-        sizes.append(len(curr_points))
-      
-      sizes = tf.convert_to_tensor(value=sizes)
-      max_num_points = tf.reduce_max(sizes)
-      
+def load_batch_of_points(filenames, batch_shape=[-1], delimiter=',', point_dimension=3, dtype=tf.float32):
+  """ Loads a batch of point clouds form the given ASCII files and creates zero padded tensor
+  for the point coordinates and the features and a 'sizes' tensor with the number of points per point cloud.
 
-      feature_dimension = features[0].shape[1]
-      for i in range(batch_size):
-        pad_size = max_num_points - sizes[i]
-        points[i] = tf.concat((points[i], tf.zeros(shape=[pad_size,point_dimension], dtype=dtype)),axis=0)
-        features[i] = tf.concat((features[i], tf.zeros(shape=[pad_size, feature_dimension], dtype=dtype)),axis=0)
-      
-      points = tf.stack(values=points, axis=0)
-      features = tf.stack(values=features, axis=0)
-      
-      points = tf.reshape(tensor=points, shape=batch_shape+[max_num_points, point_dimension])
-      features = tf.reshape(tensor=features, shape=batch_shape+[max_num_points, feature_dimension])
-      sizes = tf.reshape(tensor=sizes, shape=batch_shape)
+  Args:
+    filenames: `list` of `string`, the paths to the files
+    batch_shape: A 1D int `Tensor`
+    delimiter: `string` delimiter that separates the points in the file
+    dimension: `int` D1 , the first D1 elements in each line are treated as point coordinates, the rest as features
+    dtype: `tf.dtype` of the output tensors
 
-      return points, features, sizes
+  Returns:
+    points: A `Tensor`of shape [A1,...,An,V,D1] and type `dtype`
+    features: A `Tensor`of shape [A1,...,An,V,D2] and type `dtype`
+    sizes: A int `Tensor`of shape [A1,...,An] 
+  """
+    
+  batch_size = len(filenames)
+  if tf.reduce_prod(batch_shape)!=batch_size:
+    raise ValueError('Invalid batch shape %s for batch size %s'%(batch_shape, batch_size))
+  points = []
+  features = []
+  max_num_points = 0
+  sizes = []
+  for filename in filenames:
+    curr_points, curr_features = load_points_from_file(filename=filename, delimiter=delimiter, dtype=dtype)
+    points.append(curr_points)
+    features.append(curr_features)
+    sizes.append(len(curr_points))
+  
+  sizes = tf.convert_to_tensor(value=sizes)
+  max_num_points = tf.reduce_max(sizes)
+  
+
+  feature_dimension = features[0].shape[1]
+  for i in range(batch_size):
+    pad_size = max_num_points - sizes[i]
+    points[i] = tf.concat((points[i], tf.zeros(shape=[pad_size,point_dimension], dtype=dtype)),axis=0)
+    features[i] = tf.concat((features[i], tf.zeros(shape=[pad_size, feature_dimension], dtype=dtype)),axis=0)
+  
+  points = tf.stack(values=points, axis=0)
+  features = tf.stack(values=features, axis=0)
+  
+  points = tf.reshape(tensor=points, shape=batch_shape+[max_num_points, point_dimension])
+  features = tf.reshape(tensor=features, shape=batch_shape+[max_num_points, feature_dimension])
+  sizes = tf.reshape(tensor=sizes, shape=batch_shape)
+
+  return points, features, sizes
+
+def load_batch_of_meshes(filenames, batch_shape=[-1], file_type=None, dtype=tf.float32, **kwargs):
+  """ Loads a batch of point clouds from the given mesh files and creates a zero padded tensor
+  for the point coordinates and a 'sizes' tensor with the number of points per point cloud.
+
+  Args:
+    filenames: `list` of `string`, the paths to the files
+    batch_shape: A 1D int `Tensor` [A1,...,An]
+    filename: `string` path to the file
+    file_type: A string specifying the type of the file (e.g. 'obj', 'stl'). If
+      not specified the file_type will be inferred from the file name.
+    **kwargs: Additional arguments that should be passed to trimesh.load().
+    dtype: `tf.dtype` of the output tensors
+
+  Returns:
+    points: A `Tensor`of shape [A1,...,An,V,3] and type `dtype`
+    sizes: A int `Tensor`of shape [A1,...,An] 
+  """
+  batch_size = len(filenames)
+  points = []
+  sizes =[]
+  for filename in filenames:
+    mesh = load_mesh(filename, file_type=file_type, **kwargs)
+    curr_points = tf.convert_to_tensor(value=mesh.vertices,dtype=dtype)
+    points.append(curr_points)
+    sizes.append(len(curr_points))
+    tf.convert_to_tensor(sizes)
+  max_num_points = tf.reduce_max(sizes)
+
+  for i in range(batch_size):
+    pad_size = max_num_points - sizes[i]
+    points[i] = tf.concat((points[i], tf.zeros(shape=[pad_size,3], dtype=dtype)),axis=0)
+  points = tf.stack(values=points,axis=0)
+  points = tf.reshape(tensor=points, shape=batch_shape+[max_num_points,3])
+  sizes = tf.reshape(tensor=sizes, shape=batch_shape)
+
+  return points, sizes
