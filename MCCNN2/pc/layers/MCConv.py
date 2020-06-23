7fb3e05b1b8aa46b308c8ceaf4744c6a8b59c7f0
Michael Schelling <michael-1.schelling@uni-ulm.de>
2020-06-23 13:18:47 +0200

    movend MC-conv to layers

--- MCCNN2/pc/layers/MCConv.py
+++ MCCNN2/pc/layers/MCConv.py
@@ -166,3 +166,64 @@ class MCConv:
         convolution_result = tf.gather(convolution_result,
                                        pOutPC.sortedIndicesBatch_)
       return convolution_result
+
+
+class MonteCarloConvolution(MCConv):
+  """ Class to represent a Monte-Carlo convolution layer on one point cloud
+
+    Attributes:
+      numInFeatures_: Integer, the number of features per input point
+      numoutFeatures_: Integer, the number of features to compute
+      numHidden_: Integer, the number of neurons in the hidden layer of the
+        kernel MLP
+      numDims_: Integer, dimensionality of the point cloud
+      convName_: String, name for the operation
+  """
+
+  def __init__(self,
+               pNumInFeatures,
+               pNumOutFeatures,
+               pHiddenSize,
+               pNumDims,
+               pConvName=None):
+    """ Constructior, initializes weights
+
+    Args:
+    pNumInFeatures: Integer D_in, the number of features per input point
+    pNumOutFeatures: Integer D_out, the number of features to compute
+    pHiddenSize: Integer, the number of neurons in the hidden layer of the
+        kernel MLP
+    pNumDims: Integer, dimensionality of the point cloud
+    pConvName: String, name for the operation
+    """
+    super(self).__init__(pNumInFeatures, pNumOutFeatures, pHiddenSize,
+                         pNumDims, pConvName)
+
+  def __call__(self,
+               pInFeatures,
+               pPC: PointCloud,
+               pRadius,
+               pBandWidth=0.2,
+               return_sorted=False,
+               name=None):
+    """ Computes the Monte-Carlo Convolution
+
+    Note:
+      In the following, A1 to An are optional batch dimensions.
+      D_in is the number of input features.
+
+    Args:
+      pInFeatures: A float Tensor of shape [N,D_in] or [A1,...,An,V,D],
+        the size must be the same as the points in the input point cloud.
+      pPC: A PointCloud instance
+      pRadius: A float, the convolution radius.
+      pBandWidth: The used bandwidth used in the kernel densitiy estimation on
+        the input point cloud.
+      return_sorted: A boolean, if 'True' the output tensor is sorted
+        according to the batch_ids.
+
+      Returns:
+        Tensor with shape [N,D_out]
+    """
+    return super(self).__call__(self, pInFeatures, pPC, pPC, pRadius,
+                                pBandWidth, return_sorted, name)
