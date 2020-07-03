d81af7acbf011b246f3c198249853e97f53e50c0
Michael Schelling <michael-1.schelling@uni-ulm.de>
2020-07-03 10:45:34 +0200

    added bazel pytest of op

--- custom_ops/compile_with_bazel.sh
+++ custom_ops/compile_with_bazel.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # to test the compilation of a single op
-op="build_grid_ds"
-bazel clean
+op="compute_keys"
+# bazel clean
 bazel build 'tfg_custom_ops/'$op":python/ops/_"$op"_ops.so" --verbose_failures
+bazel test 'tfg_custom_ops/'$op:$op'_ops_py_test'
