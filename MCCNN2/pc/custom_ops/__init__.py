9433950127428908ff6b9052229a0e29cc7683f7
Michael Schelling <michael-1.schelling@uni-ulm.de>
2020-07-03 11:40:32 +0200

    now depends on pip pkg "tfg custom ops"

--- MCCNN2/pc/custom_ops/__init__.py
+++ MCCNN2/pc/custom_ops/__init__.py
@@ -1,22 +1,6 @@
-
-
 from .custom_ops_wrapper import basis_proj
 from .custom_ops_wrapper import build_grid_ds
 from .custom_ops_wrapper import compute_keys
 from .custom_ops_wrapper import compute_pdf
 from .custom_ops_wrapper import find_neighbors
 from .custom_ops_wrapper import sampling
-
-# import os
-# import sys
-
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# ROOT_MODULE_DIR = os.path.dirname(os.path.dirname(BASE_DIR))
-# sys.path.append(os.path.join(ROOT_MODULE_DIR, "tf_ops"))
-
-# from MCCNN2Module import basis_proj
-# from MCCNN2Module import build_grid_ds
-# from MCCNN2Module import compute_keys
-# from MCCNN2Module import compute_pdf
-# from MCCNN2Module import find_neighbors
-# from MCCNN2Module import sampling
