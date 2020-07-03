902c3bf020e83a3902e17e8a53cb7c8fc8a5a3ff
Michael Schelling <michael-1.schelling@uni-ulm.de>
2020-07-03 08:46:18 +0200

    fixed typo

--- custom_ops/compile_with_bazel.sh
+++ custom_ops/compile_with_bazel.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # to test the compilation of a single op
-op="find_neighbors"
+op="build_grid_ds"
 bazel clean
 bazel build 'tfg_custom_ops/'$op":python/ops/_"$op"_ops.so" --verbose_failures
